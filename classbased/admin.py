bb532457b8306bd9d2b3ff5e13ff0e117c9f9676
RishabhSaxena45 <136158506+RishabhSaxena45@users.noreply.github.com>
2023-09-23 23:27:48 +0530

    Delete classbased directory

--- classbased/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import student
-# Register your models here.
-class stuadmin(admin.ModelAdmin):
-    list_display = ['id' , 'name' , 'age' , 'course']
-admin.site.register(student,stuadmin)
--- classbased/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class studentform(forms.Form):
-    name = forms.CharField(max_length=20)
-    age = forms.IntegerField()
-    course = forms.CharField(max_length=10)
